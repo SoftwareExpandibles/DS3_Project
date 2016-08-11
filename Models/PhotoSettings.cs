1ae9fdd35ce1bae680971b67cc1e912549c7fa98
Shebi <siyabongashebi@outlook.com>
2016-08-11 16:22:32 +0200

    <PhotoSettings>

--- Models/PhotoSettings.cs
+++ Models/PhotoSettings.cs
@@ -8,7 +8,7 @@ using System.Web;
 
 namespace Models
 {
-  public  class PhotoSettings
+   public class PhotoSettings
     {
         public byte[] ConvertToBytes(HttpPostedFileBase image)
         {
