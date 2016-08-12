963370dd3502a772f1ae1b4c461ff2dd906322a2
NkahMan <nkanyisodlanie@outlook.com>
2016-08-12 12:49:44 +0200

    <updatee>

--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -55,6 +55,7 @@ namespace Data.Models
         public System.Data.Entity.DbSet<messages> messages { get; set; }
         public System.Data.Entity.DbSet<Job> Jobs { get; set; }
         public System.Data.Entity.DbSet<Applicant> Applicants { get; set; }
+        public System.Data.Entity.DbSet<Rating> Rating { get; set; }
         public System.Collections.IEnumerable Sms { get; set; }
     }
 }
